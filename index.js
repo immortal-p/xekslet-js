106f4343f94bf331541d08213b05d9879eb6cd5d
Ахмед Магомедов <immortalkil4oy@gmail.com>
2024-12-27 23:28:52 +0300

    add package

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 "use strict";
 import _ from "lodash";
 
-console.log(_.last(["one", "two"]));
+const arr = [23, 1, 3, 65, 3, 2, 34, 4];
+
+const bubbleSort = (arr) => {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const item = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = item;
+        swapped = true;
+      }
+    }
+    arr.length - 1;
+  } while (swapped);
+  return arr;
+};
+
+console.log(bubbleSort(arr));
